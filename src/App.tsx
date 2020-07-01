d7c82dc22b5daca383fc82b23053c29abd16f9c0
officiallynik <nikhilownstheworld@gmail.com>
2020-07-01 21:01:29 +0530

    release-1.0.0

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className='app'>
+            <h1>💖 Hello World!</h1>
+            <p>Welcome to your Electron application with React.</p>
+            <p>Its truely Awesome 😎.</p>
+            <p>Edit <span>src/app.tsx</span> to see changes.</p>
+        </div>
+    )
+};
+
+export default App;
